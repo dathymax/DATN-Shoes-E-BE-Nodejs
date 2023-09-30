7440315086cf96700c7f0517248316d18a663f23
datnguyen <datnv@godgroup.co>
2023-09-30 19:54:51 +0700

    edit check authenticate

--- src/routers/review/index.ts
+++ src/routers/review/index.ts
@@ -1,13 +1,25 @@
-import { isAuthenticated } from "../../middlewares";
+import { checkAuthentication } from "../../middlewares";
 import ReviewControllers from "../../controllers/review/ReviewControllers";
 import { Router } from "express";
 
 const reviewController = new ReviewControllers();
 
 export default (router: Router) => {
-    router.get("/reviews", isAuthenticated, reviewController.getAllReviews);
-    router.get("/reviews/:id", isAuthenticated, reviewController.getReviewById);
-    router.post("/reviews", isAuthenticated, reviewController.createReview);
-    router.patch("/reviews/:id", isAuthenticated, reviewController.updateReviewById);
-    router.delete("/reviews/:id", isAuthenticated, reviewController.deleteReviewById);
-}
+    router.get("/reviews", checkAuthentication, reviewController.getAllReviews);
+    router.get(
+        "/reviews/:id",
+        checkAuthentication,
+        reviewController.getReviewById
+    );
+    router.post("/reviews", checkAuthentication, reviewController.createReview);
+    router.patch(
+        "/reviews/:id",
+        checkAuthentication,
+        reviewController.updateReviewById
+    );
+    router.delete(
+        "/reviews/:id",
+        checkAuthentication,
+        reviewController.deleteReviewById
+    );
+};
--- src/routers/shoe/index.ts
+++ src/routers/shoe/index.ts
@@ -1,13 +1,21 @@
+import { checkAuthentication } from "../../middlewares";
 import ShoeControllers from "../../controllers/shoe/ShoeControllers";
 import { Router } from "express";
-import { isAuthenticated } from "../../middlewares";
 
 const shoeController = new ShoeControllers();
 
 export default (router: Router) => {
-    router.get("/shoes", isAuthenticated, shoeController.getAllShoes);
-    router.get("/shoes/:id", isAuthenticated, shoeController.getShoeById);
-    router.post("/shoes", isAuthenticated, shoeController.createShoe);
-    router.delete("/shoes/:id", isAuthenticated, shoeController.deleteShoeById);
-    router.patch("/shoes/:id", isAuthenticated, shoeController.updateShoeById);
-}
+    router.get("/shoes", checkAuthentication, shoeController.getAllShoes);
+    router.get("/shoes/:id", checkAuthentication, shoeController.getShoeById);
+    router.post("/shoes", checkAuthentication, shoeController.createShoe);
+    router.delete(
+        "/shoes/:id",
+        checkAuthentication,
+        shoeController.deleteShoeById
+    );
+    router.patch(
+        "/shoes/:id",
+        checkAuthentication,
+        shoeController.updateShoeById
+    );
+};
