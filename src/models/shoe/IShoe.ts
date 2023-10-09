131e587015b316b9d946af6d4ec66c54e4aaa087
nguyendat <nguyendat@gmail.com>
2023-10-10 00:13:57 +0700

    update

--- src/models/shoe/IShoe.ts
+++ src/models/shoe/IShoe.ts
@@ -16,19 +16,3 @@ export default interface IShoe extends Document {
     images?: IImages[];
     createDate?: Date;
 }
-
-export interface IShoeUpdate {
-    name?: string;
-    rate?: number;
-    shoeType?: string;
-    color?: string;
-    size?: number;
-    price?: number;
-    description?: string;
-    category?: string;
-    status?: string;
-    discountType?: string;
-    setDiscount?: string;
-    images?: IImages[];
-    createDate?: Date;
-}
